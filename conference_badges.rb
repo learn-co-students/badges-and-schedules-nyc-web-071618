44ffa450cc0ba4fb1e19f6ccac0d48982b2da318
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 02:41:17 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def batch_badge_creator(array)
+  array.map { |badges| "Hello, my name is #{badges}." }
+end
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map { |speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!" }
+end
+
+def printer(array)
+  batch_badge_creator(array).each { |badge| puts badge }
+  assign_rooms(array).each { |room| puts room }
+end
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
